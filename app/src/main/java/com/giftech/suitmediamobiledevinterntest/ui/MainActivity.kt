eaa488a1ae4ef959872f38cbdaa0b37996ea24c8
Galih Indra Firmansyah <galihindra2401@gmail.com>
2022-10-17 09:37:55 +0700

    Initial commit

---
+++ app/src/main/java/com/giftech/suitmediamobiledevinterntest/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.giftech.suitmediamobiledevinterntest.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.giftech.suitmediamobiledevinterntest.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
